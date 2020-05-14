751dd603d6f49dda3f871eb5a136a7b67539acf6
Paul Stahr <paul.stahr@gmx.de>
2020-05-14 18:20:37 +0200

    Added more functionality to average_image-script

--- average_images.py
+++ average_images.py
@@ -27,55 +27,48 @@ def get_index(arg,name):
     except:
         return -1
 
-def process_frame(filenames, cap, mode, expression, offset, logging):
+def process_frame(filenames, mode, criteria, expression, offset, logging):
     if len(filenames) == 0:
         return None
     if logging < 0:
         print(filenames)
     added = None
-    accepted = 0
+    accepted = []
     for file in filenames:
         img = imageio.imread(file)
+        if criteria is not None:
+            if not eval(criteria):
+                continue
         if expression is not None:
             img = eval(expression)
-        if (img == 0).all():
-            if logging < 1:
-                print("complete black frame " + file)
-        elif not np.isfinite(img).all():
-            if logging < 1:
-                print("not finite values in " + file)
+        img = img.astype(float)
+        if mode == Mode.AVERAGE_NORMALIZED or mode == Mode.VARIANCE_ARC:
+            #if np.min(np.abs(LA.norm(img, axis=2))) == 0:
+            #    plt.imshow(LA.norm(img, axis=2))
+            #    plt.show()
+            img /= LA.norm(img, axis=2)[...,None]
+            #plt.imshow(img*0.5 + 0.5)
+            #plt.show()
+        if offset is not None:
+            img -= offset
+        accepted.append(file)
+        if mode == Mode.AVERAGE or mode == Mode.AVERAGE_NORMALIZED:
+            to_add=img
+        elif mode == Mode.VARIANCE or mode == Mode.VARIANCE_NORMALIZED:
+            to_add=img ** 2
+        elif mode == Mode.VARIANCE_ARC:
+            to_add=2*np.arcsin(LA.norm(img, axis=2) / 2)
+        if added is not None:
+            added += to_add
         else:
-            if np.max(np.abs(img)) > cap:
-                print("value cap exceeded in " + file)
-            else:
-                img = img.astype(float)
-                if mode == Mode.AVERAGE_NORMALIZED or mode == Mode.VARIANCE_ARC:
-                    #if np.min(np.abs(LA.norm(img, axis=2))) == 0:
-                    #    plt.imshow(LA.norm(img, axis=2))
-                    #    plt.show()
-                    img /= LA.norm(img, axis=2)[...,None]
-                    #plt.imshow(img*0.5 + 0.5)
-                    #plt.show()
-                if offset is not None:
-                    img -= offset
-                accepted += 1
-                if mode == Mode.AVERAGE or mode == Mode.AVERAGE_NORMALIZED:
-                    to_add=img
-                elif mode == Mode.VARIANCE or mode == Mode.VARIANCE_NORMALIZED:
-                    to_add=img ** 2
-                elif mode == Mode.VARIANCE_ARC:
-                    to_add=2*np.arcsin(LA.norm(img, axis=2) / 2)
-                if added is not None:
-                    added += to_add
-                else:
-                    added = to_add
+            added = to_add
     return (added, accepted)
 
-def process_frames(filenames, cap, mode, expression, offset, logging):
+def process_frames(filenames, mode, criteria, expression, offset, logging):
     num_cores = multiprocessing.cpu_count()
     factor = (len(filenames) + num_cores - 1) // num_cores
-    image_list=Parallel(n_jobs=num_cores)(delayed(process_frame)(filenames[i * factor:min((i + 1) * factor,len(filenames))], cap, mode, expression, offset, logging) for i in range(num_cores))
-    accepted = 0
+    image_list=Parallel(n_jobs=num_cores)(delayed(process_frame)(filenames[i * factor:min((i + 1) * factor,len(filenames))], mode, criteria, expression, offset, logging) for i in range(num_cores))
+    accepted = []
     added = None
     for img in image_list:
         if img is not None and img[0] is not None:
@@ -95,11 +88,6 @@ if verbose_idx != -1:
     logging=int(sys.argv[verbose_idx +1])
 if logging < 1:
     print(sys.argv)
-
-output_idx = get_index(sys.argv, "output")
-if output_idx == -1:
-    print("no output specified")
-    exit()
 
 cap_idx = get_index(sys.argv, "cap")
 cap = np.inf
@@ -133,8 +121,14 @@ if mode_idx != -1:
         print("Value ",value," not known")
         
 expression_index = get_index(sys.argv, "expression")
+expression = None
 if expression_index != -1:
     expression = compile(sys.argv[expression_index + 1], '<string>', 'eval')
+
+criteria_index = get_index(sys.argv, "criteria")
+criteria = None
+if criteria_index != -1:
+    criteria = compile(sys.argv[criteria_index + 1], '<string>', 'eval')
 
 input_idx = get_index(sys.argv, "input")
 for arg in sys.argv[input_idx + 1:]:
@@ -147,9 +141,9 @@ if logging < 0:
     print(filenames)
 preimage = None
 if premode is not None:
-    preimage, accepted=process_frames(filenames, cap, premode, expression, None, logging)
+    preimage, accepted=process_frames(filenames, premode, criteria, expression, None, logging)
     print("accepted",accepted)
-    preimage /= accepted
+    preimage /= len(accepted)
     if show:
         plt.imshow(preimage)
         plt.show()
@@ -157,42 +151,61 @@ if premode is not None:
 image = None
 if mode is not None:
     print("second pass")
-    image, accepted = process_frames(filenames, cap, mode, expression, preimage, logging)
-    if divide:
-        image /= accepted
+    image, accepted = process_frames(filenames, mode, criteria, expression, preimage, logging)
     if mode == Mode.VARIANCE_NORMALIZED or mode == Mode.AVERAGE_NORMALIZED:
         image = LA.norm(image, axis=2)
 else:
     image = preimage
-print("image ", image.shape)
-print("accepted",accepted,"of",len(filenames))
-
+print("accepted",len(accepted),"of",len(filenames))
 
 coutput_idx = get_index(sys.argv, "coutput")
 if coutput_idx != -1:
     coutput = sys.argv[coutput_idx + 1]
-    print(coutput , accepted)
+    print(coutput , len(accepted))
     file = open(coutput,"w")
-    file.write(str(accepted))
+    file.write(str(len(accepted)))
     file.close()
-output_file = sys.argv[output_idx + 1]
 
-#if output_file.endswith('.exr'):
-#    HEADER = OpenEXR.Header(*image.shape[0:2])
-#    float_chan = Imath.Channel(Imath.PixelType(Imath.PixelType.FLOAT))
-#    HEADER['channels'] = dict([(c, float_chan) for c in "RGB"])
-#    exr = OpenEXR.OutputFile(output_file, HEADER)
-#    r = array.array('f', (image[:,:,0].flatten()).astype(np.float32)).tostring()
-#    g = array.array('f', (image[:,:,1].flatten()).astype(np.float32)).tostring()
-#    b = array.array('f', (image[:,:,2].flatten()).astype(np.float32)).tostring()
-#    exr.writePixels({'R': r, 'G': g, 'B': b})
-#else:
-image = np.where(np.isnan(image), np.zeros_like(image), image)
-imageio.imwrite(sys.argv[output_idx + 1],(image).astype(np.float32))
-image=imageio.imread(sys.argv[output_idx + 1])
-print(np.min(image), np.max(image))
-if show:
-    plt.imshow(image / 255)
-    plt.show()
+noutput_idx = get_index(sys.argv, "noutput")
+if noutput_idx != -1:
+    noutput = sys.argv[noutput_idx + 1]
+    print(noutput , len(accepted))
+    file = open(noutput,"w")
+    for file in accepted:
+        file.write(os.path.splitext(file)[0])
+    file.close()
 
-#imageio.imwrite(sys.argv[output_idx + 1],image.astype(np.uint8))
+moutput_idx = get_index(sys.argv, "moutput")
+if moutput_idx != -1:
+    moutput = sys.argv[moutput_idx + 1]
+    print(moutput , len(accepted))
+    indices = []
+    for afile in accepted:
+        indices.append(int(os.path.splitext(ntpath.basename(afile))[0]))
+    indices = np.asarray(indices) + 1
+    indices=np.sort(indices)
+    file = open(moutput,"w")
+    for idx in indices:
+        file.write(str(idx) + '\n')
+    file.close()
+
+if image is not None:
+    doutput_idx = get_index(sys.argv, "doutput")
+    image = np.where(np.isnan(image), np.zeros_like(image), image)
+    if doutput_idx != -1:
+        doutput_file = sys.argv[doutput_idx + 1]
+        if len(accepted) != 0:
+            divided = image / len(accepted)
+            imageio.imwrite(doutput_file,(divided).astype(np.float32))
+
+    output_idx = get_index(sys.argv, "output")
+    if output_idx != -1:
+        output_file = sys.argv[output_idx + 1]
+        imageio.imwrite(output_file,(image).astype(np.float32))
+        #image=imageio.imread(output_file)
+        #print(np.min(image), np.max(image))
+        #if show:
+        #    plt.imshow(image / 255)
+        #    plt.show()
+
+        #imageio.imwrite(sys.argv[output_idx + 1],image.astype(np.uint8))
