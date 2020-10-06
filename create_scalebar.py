1b7cdccc5c690e0a20ded7d94934efe792ba3cfe
Paul Stahr <paul.stahr@gmx.de>
2020-10-06 16:36:29 +0200

    Added option to export logarithmic scalebars

--- create_scalebar.py
+++ create_scalebar.py
@@ -4,7 +4,7 @@ import sys
 from matplotlib import cm
 
 if len(sys.argv) < 2 or (sys.argv[1] != 'h' and sys.argv[1] != 'v'):
-    raise Exception("Usage: <v/h> <cmap> <from> <to> <title> (<output>)")
+    raise Exception("Usage: <v/h> <cmap> <from> <to> <scale> <title> (<output>)")
 else:
     mpl.rcParams['savefig.pad_inches'] = 0
     fig, ax = plt.subplots(figsize=(6, 1) if sys.argv[1] == 'h' else (2, 6))
@@ -18,13 +18,15 @@ else:
         cmap = cm.gray
     else:
         raise Exception("Unknown cmap", sys.argv[2])
-    norm = mpl.colors.Normalize(vmin=float(sys.argv[3]), vmax=float(sys.argv[4]))
-    print(norm)
-    print(cm)
+    norm = None
+    if sys.argv[5] == "norm":
+        norm = mpl.colors.Normalize(vmin=float(sys.argv[3]), vmax=float(sys.argv[4]))
+    elif sys.argv[5] == "log":
+        norm = mpl.colors.LogNorm(vmin=float(sys.argv[3]), vmax=float(sys.argv[4]))
     fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
-                 cax=ax, orientation=('horizontal' if sys.argv[1] == 'h' else 'vertical'), label=sys.argv[5], fraction=0.046, pad=0.04)
+                 cax=ax, orientation=('horizontal' if sys.argv[1] == 'h' else 'vertical'), label=sys.argv[6], fraction=0.046, pad=0.04)
     plt.tight_layout()
-    if len(sys.argv) < 7:
+    if len(sys.argv) < 8:
         plt.show()
     else:
-        plt.savefig(sys.argv[6])
+        plt.savefig(sys.argv[7])
