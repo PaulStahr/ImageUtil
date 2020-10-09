0126fe9544776d6ff0ba28e5060bcdddcdfd9ca5
Paul Stahr <paul.stahr@gmx.de>
2020-10-09 14:26:44 +0200

    Use pyexr as a workaround for imageio

--- plot_image.py
+++ plot_image.py
@@ -2,9 +2,26 @@ import imageio
 import matplotlib.pyplot as plt
 import sys
 import numpy as np
+import pyexr
 
-img = imageio.imread(sys.argv[1])
+def read_image(file):
+    img = None
+    if file.endswith(".exr"):
+        img = pyexr.read(file)
+    else:
+        img = imageio.imread(file)
+    if len(img.shape) == 2:
+        img = img[..., None]
+    if img.shape[2] == 1:
+        img = img[:,:,0]
+    return img
+
+
+img = read_image(sys.argv[1])
 plt.imshow(img)
-if True:
-    img /= np.max(img)
-plt.imsave(sys.argv[2], img, cmap=plt.cm.jet)
+if len(sys.argv) < 3:
+    plt.show()
+else:
+    if True:
+         img /= np.max(img)
+    plt.imsave(sys.argv[2], img, cmap=plt.cm.jet)
